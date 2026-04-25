b4a70e57324095ddce9c8bc70f7618ea471a093c
mian0606 <mih037@ucsd.edu>
2026-04-24 22:19:57 -0700

    7

--- projects.js
+++ projects.js
@@ -1,4 +1,4 @@
-import { fetchJSON, renderProjects } from '../global.js';
+import { fetchJSON, renderProjects } from 'global.js';
 //data in lib
 const projects = await fetchJSON('../lib/projects.json');
 const container = document.querySelector('.projects');
